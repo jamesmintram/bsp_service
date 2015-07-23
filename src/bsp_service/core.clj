1b5db6ce6fdd1be4414e8c0ffd8193f08d2e1220
James Mintram <james@lemonmoosegames.com>
2015-07-23 19:38:15 +0100

    Refined the code some more Added some TODOs into the code

--- src/bsp_service/core.clj
+++ src/bsp_service/core.clj
@@ -4,47 +4,36 @@
             [compojure.core :refer [defroutes ANY]]
             [clojure.core.async :as async]))
 
-
-(defn process
-  [line]
-  (for [ i (range 3) ]
-    (str line i)))
-
-(defn to-s3
-  [group to-bucket]
-  (async/thread
-    (while true
-      (let [log_chunk (async/<!! to-bucket)] ))))
-
-(defn storage-consumer
+(defn storage-buffer
   "This will fill a buffer until full. When full it will
   swap to a new buffer and schedule the old buffer to be 
-  stored."
+  stored.
+  Change this to a Java Buffer which I can then dump"
   [group from-processed-messages to-bucket]
-  (async/thread
-    (while true
-      (let [buffer_size (atom 0)
-            buffer (atom ())
-            buffer_max (group :buffer)]
+  (while true
+    (let [buffer_size (atom 0)
+          buffer (atom ())
+          buffer_max (group :buffer)]
 
-        (while (< @buffer_size buffer_max)
-          (let [line (async/<!! from-processed-messages)]
-              (swap! buffer_size + (count line))
-              (swap! buffer conj line)))
-        (async/>!! to-bucket @buffer)
-        (println "Flushing Queuefor group: " (group :name))))))
+      (while (< @buffer_size buffer_max)
+        (let [line (async/<!! from-processed-messages)]
+            (swap! buffer_size + (count line))
+            (swap! buffer conj line)))
+      (async/>!! to-bucket @buffer)
+      (println "Flushing Queuefor group: " (group :name)))))
 
-(defn message-consumer
-  "This is the method which will take a message apart per group."
-  [group from-message to-buffer]
-  (async/thread
-    (while true
-      (let [line (async/<!! from-message)
-            data (process line)]
-        (doseq [current_line data]
-          (async/>!! to-buffer current_line))))))
+(defn storage-buffer-channel
+  "Takes an input channel, and returns an output channel. Data from the input channel
+  is buffered until a flush is triggered by becomming full."
+    [group input-channel]
+    (let [output-channel (async/chan)]
+    (async/thread (storage-buffer group input-channel output-channel))
+    output-channel))
 
 (defn message-fan-out
+  "Takes a single input channel and returns a list of channels - each of which will
+  recieve everything from the input channel."
+
   [input-channel output-channel-count]
   (let 
     [output-channels (repeat output-channel-count (async/chan (async/sliding-buffer 1024)))]
@@ -57,19 +46,29 @@
 
 (defn start-async-consumers
   "Start a consumer for each group which can process each message as it sees fit."
-  [groups event-sink]
+  [groups event-sink aggregator destination]
   (let 
-    [output-channels (message-fan-out event-sink (count groups))]
+    [ output-channels (message-fan-out event-sink (count groups)) ]
     (doseq [[group input-channel] (map list groups output-channels)]
-        (let 
-          [buffer-chan (async/chan) bucket-chan (async/chan)]
-          
-          (println "Starting consumer for channel: " (group :name))
-          
-          (message-consumer group input-channel buffer-chan)
-          (storage-consumer group buffer-chan bucket-chan)
-          (to-s3 group bucket-chan)))))
+        (async/thread (destination group (storage-buffer-channel group (async/map< aggregator input-channel)))))))
 
+
+(comment
+  An ETL (Extract Transform Load) application. 
+
+  TODO: Look at closing channels, docs for pipe good place to start.
+        Refactor this to use Async Pipelines - Reason I haven't yet; this is a learning excercise.
+
+  )
+(defn process
+  [line]
+  (for [ i (range 300) ]
+    (str line i)))
+
+(defn to-s3
+  [group to-bucket]
+  (while true
+    (let [log_chunk (async/<!! to-bucket)] )))
 
 (def event-sink (async/chan (async/sliding-buffer 1024)))
 (defn test_boot
@@ -79,15 +78,7 @@
               {:name "seg2" :buffer 328}
               {:name "seg3" :buffer 512}
               {:name "seg4" :buffer 128}]]
-    (start-async-consumers groups event-sink)))
-
-
-(comment 
-  chan-to-store (async/chan)
-    chan-to-bucket (async/chan)
-
-  (storage-consumer chan-to-store chan-to-bucket)
-    (to-s3 chan-to-bucket))
+    (start-async-consumers groups event-sink process to-s3)))
 
 
 (defroutes app
